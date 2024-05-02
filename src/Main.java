a124041a0595618a9ebfd95f9fbad084b9e384ad
Mehdi ÇELİK <105076539+MehdCan98@users.noreply.github.com>
2024-05-02 15:26:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    static Random random = new Random();
+    public static void main(String[] args) {
+        MatchSchedule match = new MatchSchedule();
+        match.teamInput();
+
+    }
+}
